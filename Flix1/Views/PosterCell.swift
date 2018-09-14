80911de35d9e88b64429b3a36c4c8a59c172ecca
user143116 <user143116@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-13 20:08:26 -0700

    add tab bar and superhero cells

---
+++ Flix1/Views/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  Flix1
+//
+//  Created by user143116 on 9/13/18.
+//  Copyright © 2018 MSU. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterimageView: UIImageView!
+}
